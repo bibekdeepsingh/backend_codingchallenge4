ef58ca65524077c6c6ce7e1a6fd52fcdaef4dc0c
Prabhkiran Singh <Prabhkiransingh@rrc.ca>
2025-03-03 09:50:25 -0600

    Add legacy peer in yaml

--- src/testFirebase.ts
+++
@@ -1,9 +0,0 @@
-import admin from "./config/firebase";
-
-async function verifyFirebase() {
-    const users = await admin.auth().listUsers();
-    console.log("Firebase connected. Total users:", users.users.length);
-}
-
-
-verifyFirebase().catch(console.error);
